534cb2189aa1f0592a6a359c900c7ced92651bce
issuebombom <issuebombom@gmail.com>
2023-07-28 14:50:40 +0900

    fix: add res.send logic

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,15 +8,16 @@ class UserController {
   userService = new UserService();
 
   findUser = async (req: Request, res: Response) => {
-    const { userId }: User = req.body; // 프론트에서 보낸 폼데이터를 받는다.
+    const userId: string = req.params.userId; // 프론트에서 보낸 폼데이터를 받는다.
 
     try {
       // 유저 찾기
-      const user = await this.userService.getUser(userId);
+      const user = await this.userService.getUser(Number(userId));
 
       if (!user) {
         return res.status(404).send({ message: '회원 정보가 없습니다.' });
       }
+      return res.send({ data: user });
     } catch (err) {
       if (err instanceof Error) {
         console.error(err.name, ':', err.message);
